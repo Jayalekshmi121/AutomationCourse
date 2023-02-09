43f855fd1f3be2903e5c634d25d8565db4d54901
Harikrishan S <Harikrishan S@192.168.1.37>
2023-02-09 19:06:21 +0530

    ok

--- AutomationCourse/src/main/java/com/obsqura/AutomationCourse/Base.java
+++ AutomationCourse/src/main/java/com/obsqura/AutomationCourse/Base.java
@@ -12,10 +12,69 @@ public class Base {
 		 driver.get("https://www.amazon.in/");
 
 	}
-	public static void main(String[] args) {
-		Base base =new Base();
-		base.initialiseBrowser();
-
+	public void getTitleCommand() {
+		
+		String actualTitle ="Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+		String title=driver.getTitle();
+		
+		if(title.equals(actualTitle))
+		{
+			System.out.println("Titles are equal");
+		}else 
+		{
+			System.out.println("Titles are not equal");
+		}
+		}
+	
+	public void getUrlCommand() {
+		
+		String actualUrl ="https://www.amazon.in/";
+		String url=driver.getCurrentUrl();
+		
+		if(url.equals(actualUrl))
+		{
+			System.out.println("Urls are equal");
+		}else 
+		{
+			System.out.println("Urls are not equal");
+		}
+		}
+	
+	public void getPageSourceCommand() {
+		String actualpageSource="window.ue_ihb = (window.ue_ihb || window.ueinit";
+		String pageSource=driver.getPageSource();
+		if(pageSource.contains(actualpageSource))
+		{
+			System.out.println("Page source is present");
+		}else 
+		{
+			System.out.println("Page source is not present");
+		}
+		
 	}
+	public void afterTest() {
+		//driver.close();
+		driver.quit();
+	}
+	
+	public void navigateCommands() {
+		driver.navigate().to("https://www.flipkart.com/");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+	}
+	
+	  public static void main(String[] args) { 
+		  Base base =new Base();
+		  base.initialiseBrowser();
+		  base.getTitleCommand(); 
+		  base.getUrlCommand();
+		  base.getPageSourceCommand();
+		  base.navigateCommands(); 
+		  base.afterTest();
+	  
+	  
+	  }
+	 
 
 }
